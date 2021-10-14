151dbc65c2bf310bd9be238875d4e5c4cd9c382d
Amit Bhalla <amit9815@gmail.com>
2021-10-14 12:20:09 +0530

    Game logic added

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 'use strict';
 
+let score = 20;
+let highScore = 0;
+let secretNumber;
+
+const setSecretNum = () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+};
+
+setSecretNum();
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  setSecretNum();
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = score;
+});
+
 document.querySelector('.check').addEventListener('click', () => {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  //  No Input
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number given!';
+  } else if (guess === secretNumber) {
+    //  Player Won
+
+    document.querySelector('.message').textContent = '🎉 Correct Guess!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess > secretNumber) {
+    // Guess too high
+
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        'Your guess is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose!';
+    }
+  } else if (guess < secretNumber) {
+    //  Guess to low
+
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Your guess is too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose!';
+    }
+  }
 });
